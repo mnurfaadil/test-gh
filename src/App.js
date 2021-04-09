2b2c2eda8b4bb38b9d50d69454fcb43c58945ed7
Muhammad Nurfaadil <nfadil.mu@gmail.com>
2021-04-10 00:48:59 +0700

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          COIN MARKET
+          COIN MARKET co
         </a>
       </header>
     </div>
